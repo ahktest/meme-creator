9dacdf21e354d809c59a7a577c76269563159c73
AhkTest <ahmethasan@gmail.com>
2020-10-31 23:28:12 +0300

    logo fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header(){
     return(
         <header>
-            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Problem?" />
+            <img src="http://pngimg.com/uploads/trollface/trollface_PNG48.png" alt="Problem?" />
             <p>Meme Generator</p>
         </header>
     )
